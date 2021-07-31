76569e0df7c63334e817fd12839abfdcae72178b
Yurii Timofeev <georgius@ural.ru>
2021-07-31 19:50:39 +0500

    datepicker innerstate

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,64 +1,81 @@
 <template>
-  <Activator @activate="togglePopup">
+  <Activator @activate="activate">
     <slot></slot>
   </Activator>
   <Popup v-model="active" :top="top" :left="left">
-    <CurrentDate v-model="date"/>
-    <calendar v-model="date" @click="active=false"/>
+    <CurrentDate v-model="date" />
+    <calendar v-model="date" @click="dateSelected" />
   </Popup>
 </template>
 <script>
-import '../assets/calendar.scss'
+import "../assets/calendar.scss";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import "dayjs/locale/ru";
-import calendar from './calendar.vue'
-import CurrentDate from './CurrentDate.vue'
-import Activator from './Activator.vue'
-import Popup from './Popup.vue'
+import calendar from "./calendar.vue";
+import CurrentDate from "./CurrentDate.vue";
+import Activator from "./Activator.vue";
+import Popup from "./Popup.vue";
 
 dayjs.extend(localizedFormat);
 dayjs.locale("ru");
 
 export default {
-  name: 'Datepicker',
+  name: "Datepicker",
   components: {
     calendar,
     CurrentDate,
     Activator,
-    Popup
+    Popup,
   },
   props: {
     modelValue: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
+      date: null,
       active: false,
       top: 0,
-      left: 0
-    }
+      left: 0,
+    };
+  },
+  mounted() {
+    this.date = this.modelValue;
   },
   provide() {
     return {
       getCurrentDate: this.getCurrentDate,
       setCurrentDate: this.setCurrentDate,
+      deactivate: this.deactivate,
+      commit: this.commit,
     };
   },
   methods: {
     getCurrentDate() {
-      return this.modelValue;
+      return this.date;
     },
     setCurrentDate(value) {
-      this.$emit('update:modelValue', value)
+      this.date = value;
     },
-    togglePopup({top, left}) {
-      this.top = top
-      this.left = left
-      this.active = true
-    }
-  }
-}
+    deactivate() {
+      this.active = false;
+    },
+    commit() {
+      this.$emit("update:modelValue", this.date);
+    },
+    dateSelected() {
+      this.commit();
+      this.deactivate();
+    },
+    activate({ top, left }) {
+      this.top = top;
+      this.left = left;
+      this.active = true;
+      this.date = this.modelValue;
+    },
+  },
+};
 </script>
